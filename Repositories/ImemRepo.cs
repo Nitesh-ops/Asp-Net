51f278ac49fb1ecb9dea8a42e3e5e9f5a078fa9b
Nitesh Singh <78256589+Nitesh-ops@users.noreply.github.com>
2022-07-22 11:41:08 +0530

    Add files via upload
    First Commit

---
+++ Repositories/ImemRepo.cs
@@ -0,0 +1,21 @@
+using firstWebApi.Entity;
+
+namespace firstWebApi.Repositories
+{
+    public interface ImemRepo
+    {
+        public IEnumerable<Items> getItems();
+
+        public Items getItem(int studentId);
+
+        public String postItem(Items item);
+
+        public List<Items> postItems(List<Items> items);
+
+        public List<Items> deleteItem(int id);
+
+        //  public List<Items> updateItem(int id , Items item);
+
+        public void updateItem(Items item);
+    }
+}
